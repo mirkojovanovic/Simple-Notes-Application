ed6f2feccf56d6675b14b7ade792788d2250627b
Mirko <mirkoj1996@gmail.com>
2020-02-10 03:44:50 +0100

    remove serializable from data class

--- app/src/main/java/com/mirkojovanovic/mj_noteskt/models/Note.kt
+++ app/src/main/java/com/mirkojovanovic/mj_noteskt/models/Note.kt
@@ -1,6 +1,4 @@
 package com.mirkojovanovic.mj_noteskt.models
 
-import java.io.Serializable
-
-data class Note(val title: String?, val content: String?) : Serializable
+data class Note(val title: String?, val content: String?)
 
